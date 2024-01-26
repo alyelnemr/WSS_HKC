b60170a9d58f84a9ca68dcf4340575de89f691d4
hassanyahya101 <hassanyahya101@gmail.com>
2024-01-26 21:52:49 +0300

    [MOD] send to zatca

--- account_custom/models/account_move.py
+++ account_custom/models/account_move.py
@@ -7,4 +7,9 @@ class AccountMove(models.Model):
     def button_process_edi_web_services(self):
         self.sudo().action_process_edi_web_services(with_commit=False)
 
+    def action_retry_edi_documents_error(self):
+        self.sudo()._retry_edi_documents_error_hook()
+        self.sudo().edi_document_ids.write({'error': False, 'blocking_level': False})
+        self.sudo().action_process_edi_web_services()
+
 
