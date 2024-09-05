8000f100abcfc5e8b04c993f72e4acb07aab8114
hassanyahya101 <hassanyahya101@gmail.com>
2024-09-05 08:09:18 +0300

    [ADD] adding Expected Arrival in purchase report

---
+++ purchase_custom/report/purchase_report.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import fields, models
+
+
+class PurchaseReport(models.Model):
+    _inherit = "purchase.report"
+
+
+    date_planned = fields.Datetime(string="Expected Arrival")
+
+    def _select(self):
+        return super(PurchaseReport, self)._select() + ", po.date_planned as date_planned"
+
+
+    def _group_by(self):
+        return super(PurchaseReport, self)._group_by() + ", date_planned"
