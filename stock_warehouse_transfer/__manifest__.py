a2e6009796bd432a82100190b9f4ca20c5adb35e
Hassan <hassanyahya101@gmail.com>
2024-09-28 12:37:10 +0300

    [ADD] stock transfer

---
+++ stock_warehouse_transfer/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Stock Warehouse Transfer",
+    "version": "17.1.0",
+    "author": "ENG Hassan", 
+    "summary": "Create a transfer of products",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/groups.xml",
+        "views/stock_warehouse_transfer.xml",
+        "data/ir_sequence.xml",
+        "security/ir.model.access.csv",
+    ],
+} 
